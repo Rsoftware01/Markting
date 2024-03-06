bc7be83d0ef1da696aaca9fdf7d0f84fde85be7a
Rafael Almeida <rafael.almeida2210@icloud.com>
2024-03-06 11:46:40 -0300

    jfjfjhjeahea

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,7 +6,7 @@ import os
 
 
 app = Flask(__name__)
-#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "84a79d3c4b03d8747677517aa12c1ef54b044b62"
 
@@ -14,7 +14,7 @@ database = SQLAlchemy(app)
 bcrypt =Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
-login_manager.login_view = "info3"
+login_manager.login_view = "info2"
 login_manager.login_view = "info3"
 login_manager.login_view = "resultados"
 
