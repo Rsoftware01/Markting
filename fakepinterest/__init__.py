bec71961875dbf2b9cace4fcb24b144085a1f87b
Rafael Almeida <rafael.almeida2210@icloud.com>
2024-03-06 11:31:31 -0300

    jfjfjru

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -8,7 +8,6 @@ import os
 app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mulheresevento_user:oc6NoMNYCTfvujz8pN5Ght3Bth5YsXFZ@dpg-cnk732n109ks73b6r22g-a.oregon-postgres.render.com/mulheresevento"
 app.config["SECRET_KEY"] = "84a79d3c4b03d8747677517aa12c1ef54b044b62"
 
 database = SQLAlchemy(app)
