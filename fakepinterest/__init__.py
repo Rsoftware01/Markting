442605cb0490d72d77dff850cf3c3a9c56b9ca39
Rafael Almeida <rafael.almeida2210@icloud.com>
2024-03-14 11:29:51 -0300

    parte do banco de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,19 +1,31 @@
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
 
-
+# Configurações do Flask
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "84a79d3c4b03d8747677517aa12c1ef54b044b62"
 
+# Inicialização das extensões
 database = SQLAlchemy(app)
-bcrypt =Bcrypt(app)
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
 
+# Configuração do logger para o SQLAlchemy
+logging.basicConfig()
+logger = logging.getLogger("sqlalchemy.engine")
+logger.setLevel(logging.INFO)
 
-from fakepinterest  import routes
+# Configuração do handler do logger para imprimir no console
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+# Import dos blueprints ou módulos
+from fakepinterest import routes
 
