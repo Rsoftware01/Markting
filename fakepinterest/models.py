a5cb5dc03172f5bc8c61d88a6433d3b7ea00d5ff
Rafael Almeida <rafael.almeida2210@icloud.com>
2024-03-14 18:09:41 -0300

    parte do banco de dados

--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -6,8 +6,7 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_usuario(id_usuario):
-    return Usuario.query.get (int(id_usuario))
-
+    return Usuario.query.get(int(id_usuario))
 
 class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
@@ -22,13 +21,32 @@ class Usuario(database.Model, UserMixin):
     objetivo_3a5_anos_outras = database.Column(database.String, nullable=False)
     objetivo5a10anos = database.Column(database.String, nullable=False)
     objetivo_5a10_anos_outras = database.Column(database.String, nullable=False)
-    idadehoje = database.Column(database.Float, nullable=False)  # Alterado para database.Float
-    valorinvestido = database.Column(database.String, nullable=False)
-    pouparmes = database.Column(database.String, nullable=False)
-    idadeaposentar = database.Column(database.String, nullable=False)
-    rendaaposentar = database.Column(database.String, nullable=False)
-    expectativavida = database.Column(database.String, nullable=False)
+    idadehoje = database.Column(database.Float, nullable=False)
+    valorinvestido = database.Column(database.Float, nullable=False)
+    pouparmes = database.Column(database.Float, nullable=False)
+    idadeaposentar = database.Column(database.Float, nullable=False)
+    rendaaposentar = database.Column(database.Float, nullable=False)
+    expectativavida = database.Column(database.Float, nullable=False)
     risco = database.Column(database.String, nullable=False)
+
+    def idadehoje_formatted(self):
+        return str(self.idadehoje).rstrip('0').rstrip('.') if '.' in str(self.idadehoje) else str(self.idadehoje)
+
+    def valorinvestido_formatted(self):
+        return str(self.valorinvestido).rstrip('0').rstrip('.') if '.' in str(self.valorinvestido) else str(self.valorinvestido)
+
+    def pouparmes_formatted(self):
+        return str(self.pouparmes).rstrip('0').rstrip('.') if '.' in str(self.pouparmes) else str(self.pouparmes)
+
+    def idadeaposentar_formatted(self):
+        return str(self.idadeaposentar).rstrip('0').rstrip('.') if '.' in str(self.idadeaposentar) else str(self.idadeaposentar)
+
+    def rendaaposentar_formatted(self):
+        return str(self.rendaaposentar).rstrip('0').rstrip('.') if '.' in str(self.rendaaposentar) else str(self.rendaaposentar)
+
+    def expectativavida_formatted(self):
+        return str(self.expectativavida).rstrip('0').rstrip('.') if '.' in str(self.expectativavida) else str(self.expectativavida)
+
 
     #idadehojee = database.Column(database.String, nullable=False)
     #Tempo_contribuicao = database.Column(database.String, nullable=False)
